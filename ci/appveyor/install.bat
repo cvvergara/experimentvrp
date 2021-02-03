cfedaafc3e9c8db1d6d93901c7d47fa2b9722e99
cvvergara <vicky@georepublic.de>
2021-02-02 20:06:31 -0600

    [appveyor] making it work

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -108,9 +108,11 @@ echo %PGIS_WILD_FILE%
 echo %PGIS_FILE%
 
 echo ==================================== POSTGIS
-if not exist "C:\Progra~1\PostgreSQL\%PG_VER%\%PGIS_WILD_FILE%" (
+:: if not exist "C:\Progra~1\PostgreSQL\%PG_VER%\%PGIS_WILD_FILE%" (
+if true (
     cd %APPVEYOR_BUILD_FOLDER%
-    if not exist %DOWNLOADS_DIR%\postgis-%PG_VER_NO_DOT%-binaries-%PGIS_VERSION%w%arch%gcc%GCC%.zip (
+::    if not exist %DOWNLOADS_DIR%\postgis-%PG_VER_NO_DOT%-binaries-%PGIS_VERSION%w%arch%gcc%GCC%.zip (
+    if true  (
         echo Downloading PostGIS %PGIS_VERSION%
         pushd %DOWNLOADS_DIR%
         curl -L -O -S -s http://winnie.postgis.net/download/windows/appveyor/postgis-%PG_VER_NO_DOT%-binaries-%PGIS_VERSION%w%arch%gcc%GCC%.zip
